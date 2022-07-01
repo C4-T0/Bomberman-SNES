8dc002ed9383b8072dfc9e22899fcc0760f0c275
Andrés Acosta <andres.acostahe@gmail.com>
2022-06-30 22:20:26 -0500

    Added Explosions

---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    public AnimatedSpriteRenderer start;
+    public AnimatedSpriteRenderer middle;
+    public AnimatedSpriteRenderer end;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetActiveRenderer(AnimatedSpriteRenderer renderer)
+    {
+        start.enabled = renderer == start;
+        middle.enabled = renderer == middle;
+        end.enabled = renderer == end;
+    }
+
+    public void SetDirection(Vector2 direction)
+    {
+        float angle = Mathf.Atan2(direction.y, direction.x);
+        transform.rotation = Quaternion.AngleAxis(angle * Mathf.Rad2Deg, Vector3.forward); 
+    }
+
+    public void DestroyAfter(float seconds)
+    {
+        Destroy(gameObject, seconds);
+    }
+}
