78963a4e028ed76a2374e6cb9f47a066785bd90b
C4-T0 <carlostorresh1@gmail.com>
2022-07-01 22:19:51 -0500

    Final enemys

--- Assets/Scripts/Enemy1Controller.cs
+++ Assets/Scripts/Enemy1Controller.cs
@@ -5,11 +5,21 @@ using UnityEngine;
 public class Enemy1Controller : MonoBehaviour
 {
 
+
+    private Vector2[] directions = { Vector2.down, Vector2.left, Vector2.right, Vector2.up };
+    private Vector2 direction;
+    private int directionIndex;
+    public float speed = 2f;
+
     public Animator animator;
-    
+
+    public new Rigidbody2D rigidbody { get; private set; }
+
     void Start()
     {
-        
+        directionIndex = Random.Range(0, 4);
+        direction = directions[directionIndex];
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -18,12 +28,31 @@ public class Enemy1Controller : MonoBehaviour
         
     }
 
+    private void FixedUpdate()
+    {
+        Vector2 position = rigidbody.position;
+        Vector2 translation = direction * speed * Time.fixedDeltaTime;
+
+        rigidbody.MovePosition(position + translation);
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
+
         if (other.CompareTag("DamagingObject"))
         {
             DeathSequence();
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        directionIndex = Random.Range(0, 4);
+        direction = directions[directionIndex];
+        Vector2 position = transform.position;
+        position.x = Mathf.Round(position.x);
+        position.y = Mathf.Round(position.y);
+        transform.position = position;
     }
 
     private void DeathSequence()
--- Assets/Scripts/Enemy2Controller.cs
+++ Assets/Scripts/Enemy2Controller.cs
@@ -5,17 +5,37 @@ using UnityEngine;
 public class Enemy2Controller : MonoBehaviour
 {
 
+    public int newX;
+    public int newY;
+
+    Vector2 newPos;
+
+    private float timer = 2f;
+    private float timerCd = 0;
+
     public Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-
+        newX = Random.Range(-6, 6);
+        newX = Random.Range(-5, 5);
     }
 
     // Update is called once per frame
     void Update()
     {
+        timerCd += Time.deltaTime;
+        if (timerCd >= timer)
+        {
+            newX = Random.Range(-6, 6);
+            newX = Random.Range(-5, 5);
+            newPos.x = newX;
+            newPos.y = newY;
 
+            transform.position = newPos;
+            timerCd = 0;
+
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
